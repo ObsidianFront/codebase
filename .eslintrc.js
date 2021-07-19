15362dbb274311292c17a1c425eddfc1b17b5caf
Jorge <jorge.vazquez@obsidiansoft.com>
2021-07-19 08:00:44 +0200

    add libs and config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,64 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended',
-    '@vue/typescript/recommended',
-    '@vue/prettier',
-    '@vue/prettier/@typescript-eslint'
-  ],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
-}
+	root: true,
+	env: {
+		node: true
+	},
+	extends: [
+		'plugin:vue/essential',
+		'@vue/airbnb',
+		'@vue/typescript/recommended',
+		'plugin:prettier/recommended',
+		'plugin:import/typescript'
+	],
+	plugins: ['prettier'],
+	parserOptions: {
+		ecmaVersion: 2020
+	},
+	settings: {
+		'import/resolver': {
+			typescript: {}
+		}
+	},
+	rules: {
+		indent: [2, 'tab'],
+		'no-tabs': 0,
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'import/no-extraneous-dependencies': ['error', { devDependencies: true }]
+	},
+	overrides: [
+		{
+			files: [
+				'**/__tests__/*.{j,t}s?(x)',
+				'**/tests/unit/**/*.spec.{j,t}s?(x)'
+			],
+			env: {
+				jest: true
+			}
+		},
+		{
+			files: ['*.vue'],
+			rules: {
+				'class-methods-use-this': [
+					'error',
+					{
+						exceptMethods: [
+							// vue lifecycle methods
+							'beforeCreate',
+							'created',
+							'beforeMount',
+							'mounted',
+							'beforeUpdate',
+							'updated',
+							'activated',
+							'deactivated',
+							'beforeDestroy',
+							'destroyed',
+							'errorCaptured'
+						]
+					}
+				]
+			}
+		}
+	]
+};
